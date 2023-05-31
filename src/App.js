ce461cb95d7595d765e75d77e480b776ade121af
Maximoe2588 <70680274+Maximoe2588@users.noreply.github.com>
2023-05-31 17:51:46 -0500

    updated routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import About from './components/About';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <main>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/projects" component={Projects} />
-            <Route path="/contact" component={Contact} />
-          </Switch>
+    <Router>
+      <div>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
         </main>
-      <footer>
-        <p>© 2023 Your Name</p>
-      </footer>
-    </div>
+        <footer>
+          <p>© 2023 Your Name</p>
+        </footer>
+      </div>
+    </Router>
   );
 };
-
 export default App;
--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <section id="about">
       <h2>About Me</h2>
       <p>
-        
+      I am a passionate software developer with expertise in web development. I love creating user-friendly and engaging applications using the latest technologies. With a strong foundation in HTML, CSS, and JavaScript, along with experience in frameworks like React, I strive to deliver high-quality code and exceptional user experiences.
       </p>
     </section>
   );
