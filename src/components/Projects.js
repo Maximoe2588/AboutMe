28f52ba5990bab8c1de6abd08915a327efa45a92
Maximoe2588 <70680274+Maximoe2588@users.noreply.github.com>
2023-05-31 19:47:44 -0500

    link to projects added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Projects = () => {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    const fetchRepositories = async () => {
+      try {
+        const accessToken = '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';
+
+        const response = await axios.get('https://api.github.com/users/Maximoe2588/repos', {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        setRepositories(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchRepositories();
+  }, []);
+
   return (
     <section id="projects">
       <h2>My Projects</h2>
       <ul>
-        <li>
-          <h3>Project 1</h3>
-          <p>Description of project 1.</p>
-        </li>
-        <li>
-          <h3>Project 2</h3>
-          <p>Description of project 2.</p>
-        </li>
-        <li>
-          <h3>Project 3</h3>
-          <p>Description of project 3.</p>
-        </li>
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+              {repo.name}
+            </a>
+          </li>
+        ))}
       </ul>
     </section>
   );
 };
 
 export default Projects;
+
+
+
+//'Bearer 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'
